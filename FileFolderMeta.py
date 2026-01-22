b258ce4298379f1822924a84eed6e966a98d9dab
Niema Moshiri <niemamoshiri@gmail.com>
2026-01-21 20:39:05 -0800

    Increment version

--- FileFolderMeta.py
+++ FileFolderMeta.py
@@ -17,7 +17,7 @@ import gzip
 import lzma
 
 # useful constants
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 TIMESTAMP_FORMAT_STRING = "%Y-%m-%d %H:%M:%S"
 COMPRESSED_EXTENSIONS = {'GZ', 'XZ'}
 
